faac486cd9fe4c0c187f535ff0a08387e1d457bc
VineMummy <guamelonlaox4@gmail.com>
2024-05-11 19:39:14 +0800

    添加了传送带和血条，传送带上每隔四秒出现一个随机形状的蛋糕，传送带右侧有垃圾桶，蛋糕一旦离开传送带接触垃圾桶就会消失并扣除一点血量

--- Conveyer.gd
+++ Conveyer.gd
@@ -2,19 +2,26 @@ extends Control
 class_name Conveyer
 
 var cakes_on_conveyer = []
+var temporary_cake_containers = []
 
-@onready var cake_container = $CakeContainer
-@onready var nine_patch_rect = $NinePatchRect
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("resized", Callable(self, "update_collision_shape"))
 
-func update_collision_shape():
-	nine_patch_rect.size = size
-	
+
 func add_cake(cake):
-	cake_container.add_child(cake)
+	#给每个随机生成的蛋糕生成一个control类型的container来装蛋糕（虽然好像也许可以直接加蛋糕……
+	var new_container = Control.new()
+	new_container.name = "TemporaryCakeContainer"
+	new_container.position = Vector2(0,0)
+	temporary_cake_containers.append(new_container)
+	add_child(new_container)
+	new_container.add_child(cake)
 	cakes_on_conveyer.append(cake)
-	# set a random position
-	cake.position = Vector2(randf_range(0, size.x), randf_range(0, size.y))
+
+
+func _on_child_entered_tree(node):
+	var new_position = Vector2(60,0)
+	var tween = get_tree().create_tween()
+	tween.tween_property(self,"position",new_position,0.4)
+
